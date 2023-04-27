b8c040e5a6944d3352cca28e237dc0437a415f11
NeKz <nekzor@users.noreply.github.com>
2023-04-28 00:19:05 +0100

    Remove TODOs

--- tas/Research.cs
+++ tas/Research.cs
@@ -8,14 +8,11 @@
 var gi = Ident.GameInput.Instance;
 var defragGame = gi.Minigame;
 
-// TODO: check for m_isPlaying
-
 var state = new UnityEngine.InputSystem.LowLevel.KeyboardState();
 
-// TODO: What happens if we solve the puzzle during animation? Maybe with RNG manipulation?
 if (!defragGame.menuObjectLink.paused && defragGame.m_playable && defragGame.isFullyVisible)
 {
-    // NOTE: Pause will be buffered until the animator finished running
+    // NOTE: The pause will be buffered until the animator has finished
     state.Press(UnityEngine.InputSystem.Key.Escape); // Pause
 }
 
@@ -26,20 +23,23 @@ if (defragGame.menuObjectLink.paused)
     state.Press(UnityEngine.InputSystem.Key.Enter); // Skip
 }
 
-switch (gi.state)
+if (gi.m_isPlaying)
 {
-    case DialogueManager.State.Dialogue:
-        state.Press(UnityEngine.InputSystem.Key.P); // Continue
-        break;
-    case DialogueManager.State.Choice:
-        // TODO: Insert menu down key presses based on gi.m_story.currentText
-        state.Press(UnityEngine.InputSystem.Key.Space); // Choose
-        break;
-    case DialogueManager.State.NonChoiceButton:
-        state.Press(UnityEngine.InputSystem.Key.Space); // Defrag/Stitch/End
-        break;
-    default:
-        break;
+    switch (gi.state)
+    {
+        case DialogueManager.State.Dialogue:
+            state.Press(UnityEngine.InputSystem.Key.P); // Continue
+            break;
+        case DialogueManager.State.Choice:
+            // TODO: Insert menu down key presses based on gi.m_story.currentText
+            state.Press(UnityEngine.InputSystem.Key.Space); // Choose
+            break;
+        case DialogueManager.State.NonChoiceButton:
+            state.Press(UnityEngine.InputSystem.Key.Space); // Defrag/Stitch/End
+            break;
+        default:
+            break;
+    }
 }
 
 var keyboard = UnityEngine.InputSystem.InputSystem.GetDevice<UnityEngine.InputSystem.Keyboard>();
